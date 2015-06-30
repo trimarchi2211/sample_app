f9170921561ea09f759f06dbb8b8c7ca05ffa339
trimarchi2211@gmail.com <trimarchi2211@gmail.com>
2015-06-30 18:50:14 -0400

    creating home and about pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7d8ec3703beca96e5588ea89c21ed802898db8176c2946ad107a83face0c7bebdb2b238a2a04f2c0c8bee4b7390e399872a69ccbd93172c8a4ea2a37aef34e7c'
